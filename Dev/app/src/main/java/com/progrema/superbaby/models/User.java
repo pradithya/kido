2bb8f0b6f5bcc138468108aa2e17b9b08525a34e
Pradithya Aria <pradithya.aria@gmail.com>
2014-01-23 23:55:01 +0800

    [DEV] add SecurityUtils class, update User class to support password verification

--- Dev/app/src/main/java/com/progrema/superbaby/models/User.java
+++ Dev/app/src/main/java/com/progrema/superbaby/models/User.java
@@ -2,6 +2,8 @@ package com.progrema.superbaby.models;
 
 import android.os.Parcel;
 
+import com.progrema.superbaby.util.SecurityUtils;
+
 /**
  * Created by iqbalpakeh on 21/1/14.
  */
@@ -12,6 +14,8 @@ public class User extends BaseModel {
      */
     private String userId;
     private String password; //TODO: implement password encryption
+    private String securityQuestion;
+    private String securityAnswer;
 
     /**
      * Standard basic constructor for non-parcel
@@ -43,6 +47,8 @@ public class User extends BaseModel {
         // Write each field into parcel
         parcel.writeString(userId);
         parcel.writeString(password);
+        parcel.writeString(securityQuestion);
+        parcel.writeString(securityAnswer);
 
     }
 
@@ -57,6 +63,8 @@ public class User extends BaseModel {
         // was written to the parcel
         userId = parcel.readString();
         password = parcel.readString();
+        securityQuestion = parcel.readString();
+        securityAnswer = parcel.readString();
 
     }
 
@@ -73,19 +81,61 @@ public class User extends BaseModel {
         }
     };
 
+
     public String getUserId() {
         return userId;
     }
 
+    /**
+     * change name
+     */
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    /* unsafe function
     public String getPassword() {
         return password;
+    }*/
+
+    /**
+     * used in changing password
+     * @param inputPlainText
+     */
+    public void setPassword(String inputPlainText) {
+        this.password = SecurityUtils.computeSHA1(password);
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    /**
+     * used in changing password
+     * @return
+     */
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    /**
+     * changing security question
+     */
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    /**
+     * used everytime user log in
+     * @param inputPlainText
+     * @return
+     */
+    public boolean verifyPassword(String inputPlainText){
+        return (SecurityUtils.computeSHA1(inputPlainText).compareTo(password) == 0);
+    }
+
+    /**
+     *
+     * @param inputPlainText
+     * @return
+     */
+    public boolean verifySecurityQuestion(String inputPlainText){
+        return (SecurityUtils.computeSHA1(inputPlainText).compareTo(securityAnswer) == 0);
     }
 }
