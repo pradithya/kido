f3fcf9822a833e2f33afbeade2d97ddd904d7746
IqbalPakeh <iqbalpakeh@gmail.com>
2014-08-23 06:00:23 +0800

    Use AlertDialog class for building Diaper Selection Dialog

--- Dev/app/src/main/java/com/progrema/superbaby/ui/fragment/dialog/DiaperDialog.java
+++ Dev/app/src/main/java/com/progrema/superbaby/ui/fragment/dialog/DiaperDialog.java
@@ -1,5 +1,8 @@
 package com.progrema.superbaby.ui.fragment.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -12,7 +15,11 @@ import com.progrema.superbaby.R;
 import com.progrema.superbaby.models.ActivityDiaper;
 
 public class DiaperDialog extends DialogFragment {
+
     private Callbacks mCallbacks;
+    private static final int DRY = 0;
+    private static final int WET = 1;
+    private static final int MIXED = 2;
 
     public static DiaperDialog getInstance() {
         return new DiaperDialog();
@@ -23,39 +30,47 @@ public class DiaperDialog extends DialogFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_fragment_diaper, parent, false);
-        Button buttonDry = (Button) view.findViewById(R.id.dialog_choice_dry);
-        buttonDry.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent result = new Intent();
-                result.putExtra(ActivityDiaper.DIAPER_TYPE_KEY, ActivityDiaper.DiaperType.DRY.getTitle());
-                DiaperDialog.this.mCallbacks.onDiaperChoiceSelected(0, result);
-                getDialog().dismiss();
-            }
-        });
-        Button buttonWet = (Button) view.findViewById(R.id.dialog_choice_wet);
-        buttonWet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent result = new Intent();
-                result.putExtra(ActivityDiaper.DIAPER_TYPE_KEY, ActivityDiaper.DiaperType.WET.getTitle());
-                DiaperDialog.this.mCallbacks.onDiaperChoiceSelected(0, result);
-                getDialog().dismiss();
-            }
-        });
-        Button buttonMixed = (Button) view.findViewById(R.id.dialog_choice_mixed);
-        buttonMixed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent result = new Intent();
-                result.putExtra(ActivityDiaper.DIAPER_TYPE_KEY, ActivityDiaper.DiaperType.MIXED.getTitle());
-                DiaperDialog.this.mCallbacks.onDiaperChoiceSelected(0, result);
-                getDialog().dismiss();
-            }
-        });
-        return view;
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.diaper_dialog_title)
+                .setItems(R.array.diaper_selection, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DRY:
+                                insertDryEntry();
+                                break;
+                            case WET:
+                                insertWetEntry();
+                                break;
+                            case MIXED:
+                                insertMixedEntry();
+                                break;
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.diaper_dialog_negative_button, null);
+        return builder.create();
+    }
+
+    private void insertDryEntry() {
+        Intent result = new Intent();
+        result.putExtra(ActivityDiaper.DIAPER_TYPE_KEY, ActivityDiaper.DiaperType.DRY.getTitle());
+        DiaperDialog.this.mCallbacks.onDiaperChoiceSelected(0, result);
+        getDialog().dismiss();
+    }
+
+    private void insertWetEntry() {
+        Intent result = new Intent();
+        result.putExtra(ActivityDiaper.DIAPER_TYPE_KEY, ActivityDiaper.DiaperType.WET.getTitle());
+        DiaperDialog.this.mCallbacks.onDiaperChoiceSelected(0, result);
+        getDialog().dismiss();
+    }
+
+    private void insertMixedEntry() {
+        Intent result = new Intent();
+        result.putExtra(ActivityDiaper.DIAPER_TYPE_KEY, ActivityDiaper.DiaperType.MIXED.getTitle());
+        DiaperDialog.this.mCallbacks.onDiaperChoiceSelected(0, result);
+        getDialog().dismiss();
     }
 
     public static interface Callbacks {
