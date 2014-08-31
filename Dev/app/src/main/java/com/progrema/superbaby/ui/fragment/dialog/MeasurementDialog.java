d08f6f0960b4275db2473d602cd68c577da50edd
IqbalPakeh <iqbalpakeh@gmail.com>
2014-09-01 00:42:46 +0800

    Use AlertDialog class on Measurement Dialog input

--- Dev/app/src/main/java/com/progrema/superbaby/ui/fragment/dialog/MeasurementDialog.java
+++ Dev/app/src/main/java/com/progrema/superbaby/ui/fragment/dialog/MeasurementDialog.java
@@ -1,12 +1,13 @@
 package com.progrema.superbaby.ui.fragment.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,7 +16,10 @@ import com.progrema.superbaby.provider.BabyLogContract;
 import com.progrema.superbaby.util.FormatUtils;
 
 public class MeasurementDialog extends DialogFragment {
+
     private Callbacks mCallbacks;
+    private String height;
+    private String weight;
 
     public static MeasurementDialog getInstance() {
         return new MeasurementDialog();
@@ -25,40 +29,50 @@ public class MeasurementDialog extends DialogFragment {
         mCallbacks = listener;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.dialog_fragment_measurement, parent, false);
-        Button ok = (Button) view.findViewById(R.id.button_ok);
-        ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText inputHeight = (EditText) getDialog().findViewById(R.id.entry_text_height);
-                String height = inputHeight.getText().toString();
-                if (!FormatUtils.isValidNumber(height)) {
-                    Toast invalidNumber =
-                            Toast.makeText(getActivity(), "invalid number", Toast.LENGTH_LONG);
-                    invalidNumber.show();
-                    return; //invalid height
-                }
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View view = inflater.inflate(R.layout.dialog_fragment_measurement, null);
+        builder.setView(view);
+        builder.setTitle(R.string.measurement_dialog_title)
+                .setNegativeButton(R.string.measurement_dialog_negative_button, null)
+                .setPositiveButton(R.string.measurement_dialog_positive_button,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                getHeight();
+                                getWeigh();
+                                if (!checkHeightAndWeight()) return;
+                                submitAndDismissDialog();
+                            }
+                        });
+        return builder.create();
+    }
 
-                EditText inputWeight = (EditText) getDialog().findViewById(R.id.entry_text_weight);
-                String weight = inputWeight.getText().toString();
-                if (!FormatUtils.isValidNumber(height)) {
-                    Toast invalidNumber =
-                            Toast.makeText(getActivity(), "invalid number", Toast.LENGTH_LONG);
-                    invalidNumber.show();
-                    return; //invalid weight
-                }
+    private void getHeight() {
+        EditText inputHeight = (EditText) getDialog().findViewById(R.id.entry_text_height);
+        height = inputHeight.getText().toString();
+    }
 
-                Intent result = new Intent();
-                result.putExtra(BabyLogContract.Measurement.HEIGHT, height);
-                result.putExtra(BabyLogContract.Measurement.WEIGHT, weight);
-                MeasurementDialog.this.mCallbacks.onMeasurementChoiceSelected(0, result);
-                getDialog().dismiss();
-            }
-        });
+    private void getWeigh() {
+        EditText inputWeight = (EditText) getDialog().findViewById(R.id.entry_text_weight);
+        weight = inputWeight.getText().toString();
+    }
 
-        return view;
+    private boolean checkHeightAndWeight() {
+        if (!FormatUtils.isValidNumber(height) || !FormatUtils.isValidNumber(weight)) {
+            Toast invalidNumber = Toast.makeText(getActivity(), "invalid number", Toast.LENGTH_LONG);
+            invalidNumber.show();
+            return false;
+        }
+        return true;
+    }
+
+    private void submitAndDismissDialog() {
+        Intent result = new Intent();
+        result.putExtra(BabyLogContract.Measurement.HEIGHT, height);
+        result.putExtra(BabyLogContract.Measurement.WEIGHT, weight);
+        MeasurementDialog.this.mCallbacks.onMeasurementChoiceSelected(0, result);
+        getDialog().dismiss();
     }
 
     public static interface Callbacks {
